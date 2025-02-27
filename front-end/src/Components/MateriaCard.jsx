ce7cd55e181aeb02aee4fd3ed01c056fe21f0dd5
gero04 <gveranovillo@gmail.com>
2025-02-26 22:14:43 -0300

    trabajando en hacer las MateriaCard en modals

--- front-end/src/Components/MateriaCard.jsx
+++ front-end/src/Components/MateriaCard.jsx
@@ -7,14 +7,47 @@ function MateriaCard({ materia }) {
     setModalCard(!modalCard);
   };
 
+  if(modalCard) {
+    document.body.classList.add('active-modal')
+  } else {
+    document.body.classList.remove('active-modal')
+  }
+
   return (
-    <div className="bg-gray-800 text-white p-4 m-4 rounded-lg inline-block">
+    <>
+    <button onClick={toggleModal} className="bg-gray-800 text-white p-4 m-4 rounded-lg inline-block">
+    <h3 className="text-xl font-bold capitalize">
+        {materia.nombreCorto}
+        </h3>
+    </button>
+
+    {modalCard && 
+      <div className="modal">
+
+        <div onClick={toggleModal} className="overlay" />
+        <div className="modal-content">
+          <h2>Hello Modal</h2>
+          <button onClick={toggleModal} className="close-modal">CLOSE</button>
+        </div>
+
+
+      </div>
+    }
+    </>
+  );
+}
+
+export default MateriaCard;
+
+
+{/* 
+  <>
+
+<div className="bg-gray-800 text-white p-4 m-4 rounded-lg inline-block">
       <h3 className="text-xl font-bold capitalize">
         #{materia.id} . {materia.nombreCorto}
       </h3>
       <p className="text-gray-500 text-sm">{materia.nombreCompleto}</p>
     </div>
-  );
-}
 
-export default MateriaCard;
+  </> */}
