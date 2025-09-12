a5b697f7c2f3d21d20e9a1d6d2f15b10fc56dc67
gero04 <gveranovillo@gmail.com>
2025-09-12 20:10:10 -0300

    feature: schema de materias, ademas añadi los archivos de schema de progreso, y el service de progreso

---
+++ back-end/src/models/materias.model.js
@@ -0,0 +1,64 @@
+const materiasSchema = {
+    id: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    nombreCorto: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    nombreCompleto: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    integradora: {
+        type: Boolean,
+        required: true,
+        unique: false
+    },
+    cargaHoraria: {
+        type: Number,
+        min: 0,
+        max: 10,
+        required: true
+    },
+    nivel: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    correlativasRegulares: {
+        type: [Number],
+        required: true,
+        unique: false
+    },
+    correlativasAprobadas: {
+        type: [Number],
+        required: true,
+        unique: false
+    },
+    modalidad: {
+        type: String,
+        enum: ['Anual', 'Cuatrimestral'],
+        required: true
+    },
+    area: {
+        type: String,
+        required: true,
+        unique: false
+    },
+    bloqueDeConocimiento: {
+        type: String,
+        required: true,
+        unique: false
+    },
+    objetivo: {
+        type: String,
+        required: true,
+        unique: false
+    }
+}
